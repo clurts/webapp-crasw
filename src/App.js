59a39ef7e11a184e6a334e31b4d23dd5e3085675
clurts <clu@rts.dk>
2021-01-28 14:40:21 +0100

    add routing and subpage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Router } from '@reach/router'
+import Home from "./components/Home"
+import Page from "./components/Page"
 import OneSignal from 'react-onesignal';
+
 
 OneSignal.initialize('e5b94a5a-a3b9-4f0b-b5a1-54d44283e640', {
   safari_web_id: "web.onesignal.auto.3a3b4186-8f32-4bbf-a810-be3f3be590a9"
@@ -9,22 +11,10 @@ OneSignal.initialize('e5b94a5a-a3b9-4f0b-b5a1-54d44283e640', {
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Home path="/" />
+      <Page path="/page" />
+    </Router>
   );
 }
 
