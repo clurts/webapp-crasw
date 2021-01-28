ea9a0c2113ccb18a068ab660eb371a9c6ad30407
clurts <clu@rts.dk>
2021-01-28 11:28:21 +0100

    change build from inject to generate

--- build.js
+++ build.js
@@ -1,11 +1,34 @@
-const {injectManifest} = require('workbox-build');
+const workboxBuild = require('workbox-build');
 
-const swSrc = 'src/custom-service-worker.js';
-const swDest = 'build/service-worker.js';
-injectManifest({
-  swSrc,
-  swDest,
-  // Other configuration options...
-}).then(({count, size}) => {
-  console.log(`Generated ${swDest}, which will precache ${count} files, totaling ${size} bytes.`);
-});
+// NOTE: This should be run *AFTER* all your assets are built
+const buildSW = () => {
+  // This will return a Promise
+  return workboxBuild.generateSW({
+    globDirectory: 'build',
+    globPatterns: [
+      '**/*.{html,json,js,css}',
+    ],
+    swDest: 'build/service-worker.js',
+
+    // Define runtime caching rules.
+    runtimeCaching: [{
+      // Match any request that ends with .png, .jpg, .jpeg or .svg.
+      urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+
+      // Apply a cache-first strategy.
+      handler: 'CacheFirst',
+
+      options: {
+        // Use a custom cache name.
+        cacheName: 'fulstaendigtobskurt',
+
+        // Only cache 10 images.
+        expiration: {
+          maxEntries: 10,
+        },
+      },
+    }],
+  });
+};
+
+buildSW();
