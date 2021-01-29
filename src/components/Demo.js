137e82d2aa60bd03d20586d0c85322b35c79b7c9
clurts <clu@rts.dk>
2021-01-29 11:16:27 +0100

    add geolocation

---
+++ src/components/Demo.js
@@ -0,0 +1,27 @@
+import { geolocated } from "react-geolocated"
+
+const Demo = ({
+    isGeolocationAvailable,
+    isGeolocationEnabled,
+    coords
+}) => {
+    return !isGeolocationAvailable ? (
+        <div>Your browser does not support Geolocation</div>
+    ) : !isGeolocationEnabled ? (
+        <div>Geolocation is not enabled</div>
+    ) : coords ? ( 
+        <div>
+            <p>Latitude: {coords.latitude}</p>
+            <p>Longtitude: {coords.longitude}</p>
+        </div>
+    ) : (
+        <div>Getting the location data&hellip; </div>
+    )
+}
+ 
+export default geolocated({
+    positionOptions: {
+        enableHighAccuracy: false,
+    },
+    userDecisionTimeout: 10000,
+})(Demo);
