7f4722acfc64f0529642b9c7d8e24858130ae0e6
clurts <clu@rts.dk>
2021-01-30 13:14:28 +0100

    set createHandlerBoundToUrl back to index.html

--- src/service-worker.js
+++ src/service-worker.js
@@ -44,7 +44,7 @@ registerRoute(
 
     return true;
   },
-  createHandlerBoundToURL(process.env.PUBLIC_URL + '/offline.html')
+  createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html')
 );
 
 
