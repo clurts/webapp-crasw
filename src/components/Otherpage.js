26256c23a90cf2aab5bc75304496a45b3390648e
clurts <clu@rts.dk>
2021-01-30 17:54:53 +0100

    update alt text due to a11y error

--- src/components/Otherpage.js
+++ src/components/Otherpage.js
@@ -5,7 +5,7 @@ const Otherpage = () => {
             <h2>Dette er otherpage.</h2>
             <p>with an image from an external ressource</p>
 
-            <img src="https://miro.medium.com/max/3703/1*jFyawcsqoYctkTuZg6wQ1A.jpeg" alt="external image"/>
+            <img src="https://miro.medium.com/max/3703/1*jFyawcsqoYctkTuZg6wQ1A.jpeg" alt="external monkey"/>
         </div>
      );
 }
