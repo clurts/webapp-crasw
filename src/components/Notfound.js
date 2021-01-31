7843461b92a9465193b58d879c834822c0dfa71b
clurts <clu@rts.dk>
2021-01-31 15:35:11 +0100

    fix styling

--- src/components/Notfound.js
+++ src/components/Notfound.js
@@ -1,8 +1,13 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react"
 
+const styleHeadline = css`
+    font-size: 4em;
+`
 const Notfound = () => {
     return (
         <div>
-            <h2 style="font-size: 4em;">Dette er offlinesiden.</h2>
+            <h2 css={styleHeadline}>Dette er offlinesiden.</h2>
             <p>Har du mistet forbindelsen..? <br/>Denne side kan ikke vises med mindre du er online.</p>
             <p><small>Eller måske har du fundet en side, som slet ikke findes...</small></p>
             <p><a href="/">Klik her</a> for at komme tilbage til forsiden.</p>
