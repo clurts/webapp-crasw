8ecc9543b656fcd9dca46e608d850d9ad98d90e7
clurts <clu@rts.dk>
2021-01-31 15:27:19 +0100

    font size on Notfound headline

--- src/components/Notfound.js
+++ src/components/Notfound.js
@@ -2,7 +2,7 @@
 const Notfound = () => {
     return (
         <div>
-            <h2>Dette er offlinesiden.</h2>
+            <h2 style="font-size: 4em;">Dette er offlinesiden.</h2>
             <p>Har du mistet forbindelsen..? <br>Denne side kan ikke vises med mindre du er online.</p>
             <p><small>Eller måske har du fundet en side, som slet ikke findes...</small></p>
             <p><a href="/">Klik her</a> for at komme tilbage til forsiden.</p>
