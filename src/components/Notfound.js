c831137e50f90f1cd766978663bd04d85a3d8b9a
clurts <clu@rts.dk>
2021-01-31 15:28:19 +0100

    fix break tag on Notfound

--- src/components/Notfound.js
+++ src/components/Notfound.js
@@ -3,7 +3,7 @@ const Notfound = () => {
     return (
         <div>
             <h2 style="font-size: 4em;">Dette er offlinesiden.</h2>
-            <p>Har du mistet forbindelsen..? <br>Denne side kan ikke vises med mindre du er online.</p>
+            <p>Har du mistet forbindelsen..? <br/>Denne side kan ikke vises med mindre du er online.</p>
             <p><small>Eller måske har du fundet en side, som slet ikke findes...</small></p>
             <p><a href="/">Klik her</a> for at komme tilbage til forsiden.</p>
         </div>
