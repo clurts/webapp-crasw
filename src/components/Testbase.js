917beba82d585f4b3e6e9a2397b7966702b253fc
clurts <clu@rts.dk>
2021-02-04 15:17:37 +0100

    remove demo code from Testbase.js

--- src/components/Testbase.js
+++ src/components/Testbase.js
@@ -10,23 +10,15 @@ let myDatabase = new Localbase('myDatabase')
     const insertIntoDb = () => {
         myDatabase.collection('users').add({
             id: 2,
-            name: 'John',
+            name: 'Jong',
             age: 58
           })
     }
 
     const getfromdb = () => {
-        const [totalDistance, setTotalDistance] = useState(0)
         myDatabase.collection('users').get().then(users => {
-           ''
-            users.map((postition, index, array) => {
-                 console.log(user.name)
-                 if(index !== array.length-1) {
-                     //Her kan du udregne afstanden mellem postition og index+1
-                    console.log("next name is: ", array[index+1].name)
-                 }
-                 
-            })
+            
+                 console.log(users)
             
         })
     }
