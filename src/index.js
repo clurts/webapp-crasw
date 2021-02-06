c84238f68e61c8ee905b1afd486a954742c8cdfd
clurts <clu@rts.dk>
2021-02-06 10:57:39 +0100

    remove context import

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
-import OneSignalContextProvider from "./contexts/OneSignalContext";
+//import OneSignalContextProvider from "./contexts/OneSignalContext";
 
 ReactDOM.render(
     <React.StrictMode>
