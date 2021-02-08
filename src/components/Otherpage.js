3acdf1d6f18cc136b8064764deb1a7b8f60282d5
clurts <clu@rts.dk>
2021-02-08 12:57:51 +0100

    cleanup otherpage.js - remove unused code

--- src/components/Otherpage.js
+++ src/components/Otherpage.js
@@ -20,27 +20,18 @@ const Otherpage = () => {
             body: JSON.stringify({
                 "app_id": "e5b94a5a-a3b9-4f0b-b5a1-54d44283e640",
                 "include_player_ids": [user.id],
-                "headings": {"en": "Heading in english"},
-                "contents": {"en": "This is the english message"}
+                "headings": {
+                    "en": "Heading in english", 
+                    "da":"Dansk overskrift"
+                },
+                "contents": {
+                    "en": "This is the english message", 
+                    "da": "Her er den danske besked..."
+                }
               })
         })
         .then(response => console.log(response))
         })
-        /*fetch("https://onesignal.com/api/v1/notifications", {
-            mode: "no-cors",
-            method: "POST", 
-            headers: {
-                "Content-Type":"application/json",
-                "Authorization":"Basic NTNhOTgzODktZWJkMS00NjcwLWE5ODAtZmJlNTg3NmM0NTk4"
-            },
-            body: JSON.stringify({
-                "app_id": "e5b94a5a-a3b9-4f0b-b5a1-54d44283e640",
-                "include_player_ids": ["2c8eaf24-bdc0-4d95-afaa-7b8a36188f8f"],
-                "headings": {"en": "Heading in english"},
-                "contents": {"en": "This is the english message"}
-              })
-        })
-        .then(response => console.log(response))*/
     }
 
 
